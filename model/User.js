5efcec06035a5bc7ebaf0ea120f1a3fbb5a1a678
nima-abdpour <nimaabdpoor@gmail.com>
2023-05-22 21:59:52 +0330

    User model created.

---
+++ model/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+const {Schema} = require("mongoose");
+const uniqueValidator = require('mongoose-unique-validator')
+const schema = mongoose.Schema
+
+const userSchema = new Schema({
+    id: String,
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: String,
+    role: ["User", "Admin", "SuperAdmin"],
+    assignedCoins: {
+        type: Number,
+        value: 0,
+    },
+    receivedCoins: {
+        type: Number,
+        value: 0
+    }
+})
+
+userSchema.plugin(uniqueValidator)
+
+const User = mongoose.model("user", userSchema)
+
+module.exports = User
