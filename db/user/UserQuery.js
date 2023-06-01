c7fd6987e621eb4c3fa73f657ef00e4301aa0b3b
nima-abdpour <nimaabdpoor@gmail.com>
2023-06-01 11:53:33 +0330

    hadle errors in User query

---
+++ db/user/UserQuery.js
@@ -0,0 +1,21 @@
+const user = require("../../model/User")
+
+async function createUser(_user) {
+    try {
+        await user.create({
+            username: _user.username,
+            password: _user.password,
+            role: _user.role
+        });
+        return {body: {success: true}, statusCode: 200};
+    } catch (error) {
+        if (error.code === 11000) {
+            return {body: {error: "User already exists. try another UserName."}, statusCode: 401, success: false}
+        } else {
+            console.error("UserQuery.js" + error.name + error.code + "error:", error);
+            return {body: {error: error.message}, success: false, statusCode: 500};
+        }
+    }
+}
+
+module.exports = createUser
