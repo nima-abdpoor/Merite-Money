94c5a31ed02d0322ae79e775d5bbca8f287ae73e
nima-abdpour <nimaabdpoor@gmail.com>
2023-06-01 11:53:04 +0330

    creating user with post request

--- controller/user/UserController.js
+++ controller/user/UserController.js
@@ -3,49 +3,49 @@ const KoaRouter = require("koa-router")
 const parser = require("koa-bodyparser")
 const user = require("../../model/User")
 const {_explicitStatus} = require("koa/lib/response");
+const createUser = require("../../db/user/UserQuery")
 
-function createUser(body) {
-
-}
-
-function init(router) {
-    router.post("/:userId/user", (context, next) => {
-       user.find({id: context.params.userId}, function(err, _user){
-           if (err){
-               context.body = "User Not Found"
-               return context.status = 404
-           }else{
-               if (context.request.body.username === undefined &&
-                   context.request.body.password === undefined &&
-                   context.request.body.roles === undefined
-               ) {
-                   context.body = "define username, password and roles"
-                   return context.status = 401
-               } else {
-                   let _role = context.body.role
-                   if (_user.roles.contains("SuperAdmin")){
-                       if (_role === "SuperAdmin"){
-                           context.body = "Not Allowed"
-                           return context.status = 403
-                       }else{
-                           if (_role === "Admin" || _role === "User") {
-                               user.create({
-                                   username: context.body.username,
-                                   password: context.body.password,
-                                   role: [_role],
-
-                               }).then((results) => {
-                                   return {success: true}
-                               })
-                           } else {
-                               context.body = "Role Is Not Valid"
-                               return context.status = 401
-                           }
-                       }
-                   }
-               }
-           }
-        })
+async function init(router) {
+    router.post("/:userId/user", async (context, next) => {
+        try {
+            const _user = await user.find({username: context.params.userId})
+            if (!"username" in _user) {
+                context.body = "User Not Found"
+                return context.status = 404
+            }
+            if (context.request.body.username === undefined &&
+                context.request.body.password === undefined &&
+                context.request.body.roles === undefined
+            ) {
+                context.body = "define username, password and roles"
+                return context.status = 401
+            } else {
+                let _role = context.request.body.role
+                if (_user[0].role.includes("SuperAdmin")) {
+                    if (_role === "SuperAdmin") {
+                        context.body = "Not Allowed"
+                        return context.status = 403
+                    } else {
+                        if (_role === "Admin" || _role === "User") {
+                            let creationUserResult = await createUser({
+                                username: context.request.body.username,
+                                password: context.request.body.password,
+                                role: [_role],
+                            })
+                            context.body = creationUserResult.body
+                            context.status = creationUserResult.statusCode
+                        } else {
+                            context.body = {error: "Role Is Not Valid"}
+                            return context.status = 401
+                        }
+                    }
+                }
+            }
+        } catch (error) {
+            console.log("Error In UserController:", error)
+            context.status = 500
+            context.body = error
+        }
     })
 }
 
