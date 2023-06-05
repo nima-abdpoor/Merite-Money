893f569100dc3143ef491c72a0d9f9157befa775
nima-abdpour <nimaabdpoor@gmail.com>
2023-06-05 09:50:43 +0330

    check hash password.

--- controller/user/UserController.js
+++ controller/user/UserController.js
@@ -3,7 +3,8 @@ const KoaRouter = require("koa-router")
 const parser = require("koa-bodyparser")
 const user = require("../../model/User")
 const {_explicitStatus} = require("koa/lib/response");
-const { createUser, updateUserRole } = require("../../db/user/UserQuery")
+const {createUser, updateUserRole} = require("../../db/user/UserQuery")
+const isPasswordMatches = require("../util/PasswordDecryption")
 
 async function init(router) {
     router.post("/:userId/user", async (context, next) => {
@@ -13,15 +14,26 @@ async function init(router) {
                 context.body = "User Not Found"
                 return context.status = 404
             }
-            if (context.request.body.username === undefined &&
-                context.request.body.password === undefined &&
-                context.request.body.roles === undefined
+            isPasswordMatches(context.request.body.password, requester[0].password, (error, isMatch) => {
+                if (error) {
+                    context.body = "Error Accrued!" + error
+                    return context.status = 500
+                } else {
+                    if (!isMatch) {
+                        context.body = "Incorrect Password!"
+                        return context.status = 401
+                    }
+                }
+            });
+            if (context.request.body.user.username === undefined &&
+                context.request.body.user.password === undefined &&
+                context.request.body.user.roles === undefined
             ) {
                 context.body = "define username, password and roles"
                 return context.status = 401
             } else {
-                let _role = context.request.body.role
-                const _user = await user.find({username: context.request.body.username})
+                let _role = context.request.body.user.role
+                const _user = await user.find({username: context.request.body.user.username})
                 if (!_user[0] || !"username" in _user) {
                     // user in body is not in database
                     if (requester[0].role.includes("SuperAdmin")) {
@@ -31,8 +43,8 @@ async function init(router) {
                         } else {
                             if (_role === "Admin" || _role === "User") {
                                 let creationUserResult = await createUser({
-                                    username: context.request.body.username,
-                                    password: context.request.body.password,
+                                    username: context.request.body.user.username,
+                                    password: context.request.body.user.password,
                                     role: [_role],
                                 })
                                 context.body = creationUserResult.body
@@ -46,8 +58,8 @@ async function init(router) {
                     if (requester[0].role.includes("Admin")) {
                         if (_role === "User") {
                             let creationUserResult = await createUser({
-                                username: context.request.body.username,
-                                password: context.request.body.password,
+                                username: context.request.body.user.username,
+                                password: context.request.body.user.password,
                                 role: [_role],
                             })
                             context.body = creationUserResult.body
@@ -62,11 +74,16 @@ async function init(router) {
                     if (!_user[0].role.includes("Admin") || !_user[0].role.includes("User")) {
                         if (((_role === "User" || _role === "Admin") && requester[0].role.includes("SuperAdmin")) ||
                             ((_role === "User") && requester[0].role.includes("Admin"))) {
-                            let roles = _user[0].role
-                            roles.push(_role)
-                            let updateUserResult = await updateUserRole(context.request.body.username, roles)
-                            context.body = updateUserResult.body
-                            context.status = updateUserResult.statusCode
+                            if (!_user[0].role.includes(_role)) {
+                                let roles = _user[0].role
+                                roles.push(_role)
+                                let updateUserResult = await updateUserRole(context.request.body.user.username, roles)
+                                context.body = updateUserResult.body
+                                context.status = updateUserResult.statusCode
+                            } else {
+                                context.body = {error: "User Already Has This Role!"}
+                                context.status = 401
+                            }
                         } else {
                             context.body = {error: "Not Allowed."}
                             return context.status = 403
