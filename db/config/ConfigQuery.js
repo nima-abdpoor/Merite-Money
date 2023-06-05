20cbfee0eee900d7375ddfbbd9a9e4c6f491e693
nima-abdpour <nimaabdpoor@gmail.com>
2023-06-05 11:07:54 +0330

    create config and get config

---
+++ db/config/ConfigQuery.js
@@ -0,0 +1,30 @@
+const config = require("../../model/Config")
+const {body} = require("koa/lib/response");
+
+async function createConfig(_config) {
+    try {
+        await config.create({
+            assignedCoins: _config.assignedCoins
+        });
+        return {body: {success: true}, statusCode: 200};
+    } catch (error) {
+        console.error("ConfigQuery.js" + error.name + error.code + "error:", error);
+        return {body: {error: error.message}, success: false, statusCode: 500};
+    }
+}
+
+async function getConfig() {
+    try {
+        let result = await config.find().limit(1).sort({$natural: -1})
+        return {body: result, statusCode: 200, success: true};
+    } catch (error) {
+        console.error("ConfigQuery.js" + error.name + error.code + "error:", error);
+        return {body: {error: error.message}, success: false, statusCode: 500};
+    }
+}
+
+
+module.exports = {
+    createConfig,
+    getConfig
+}
